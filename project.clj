fd4fa5db38107a35fe9b0e5df72cb0e652f2fa81
Andrew Phillips <theasp@gmail.com>
2016-07-10 14:09:13 -0300

    Convert to cljc, define floor

--- project.clj
+++ project.clj
@@ -1,6 +1,5 @@
-(defproject perlin "1.0.0-SNAPSHOT"
-  :description "converting the reference implementation from Java to Clojure "
-  :dependencies [[org.clojure/clojure "1.1.0"]
-                 [org.clojure/clojure-contrib "1.1.0"]]
-  :dev-dependencies [[lein-javac "0.0.2-SNAPSHOT"]
-                     [leiningen/lein-swank "1.1.0"]])
+(defproject perlin "1.1.0-SNAPSHOT"
+  :description "Perlin noise function"
+  :dependencies [[org.clojure/clojure "1.7.0"]]
+  :dev-dependencies [[leiningen/lein-swank "1.1.0"]]
+  :java-source-paths ["src"])
